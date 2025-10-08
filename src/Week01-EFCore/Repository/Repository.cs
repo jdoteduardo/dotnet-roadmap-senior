fc7e17e8f5a64c1f0bc1ce1f48b3528ccf067407
jdoteduardo <joseeduardo0131@gmail.com>
2025-10-07 23:44:43 -0300

    feat: add generic repository

---
+++ src/Week01-EFCore/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Week01_EFCore.Context;
+
+namespace Week01_EFCore.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+
+            return entity;
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+
+            return Task.FromResult(entity);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            if (entity == null)
+                return false;
+
+            _context.Set<T>().Remove(entity);
+
+            return true;
+        }
+    }
+}
